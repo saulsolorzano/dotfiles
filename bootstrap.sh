5efa985e340450532b0f21a3bf28f208d9b79b13
Saúl Solórzano <hola@saulsolorzano.com>
2021-01-03 18:47:49 -0300

    Sección de git

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,26 @@
 # Algunas de estas funciones están copiadas del archivo de Dries
 # https://github.com/driesvints/dotfiles/blob/main/fresh.sh
 
-echo "Configurando nuevos dotfiles"
-
+echo "Configurando nuevos dotfiles para oh-my-zsh"
+echo ""
 # Elimina el .zshrc de $HOME (si existe) y crea symlinks
 # del .zshrc con el archivo en nuestros .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
+echo ""
+echo "Clonamos el único plugin externo que siempre uso"
+echo ""
 # Clonamos el único plugin externo que siempre uso
 git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.dotfiles/plugins/zsh-autosuggestions
+
+echo ""
+echo "Configurando otros dotfiles"
+echo ""
+
+# Sincroniamos nuestros archivos git con los de la carpeta raíz
+rsync .gitconfig .gitignore -avh --no-perms ~;
+
+# Especificamos a nuestro git el archivo global
+git config --global core.excludesfile ~/.gitignore
 
 ./private.sh
